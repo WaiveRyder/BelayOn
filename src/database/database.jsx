1faba4184a84410eab603d9a09787659705713a4
WaiveRyder <nzh.wolf@gmail.com>
2026-02-05 14:28:34 -0700

    Implemented entrylookup and database to the router with correct pages injected

--- src/database/database.jsx
+++ src/database/database.jsx
@@ -1,9 +1,88 @@
 import React from 'react';
+import './database.css';
 
 export function Database() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>database displayed here</div>
-    </main>
+          <main>
+            <h1>Database</h1>
+
+            <p>Logged in as: User. All edits will be logged under this name.</p>
+
+            <form id="search" method="get">
+                <input type="search" class="form-control" id="search-box" required placeholder="Name or Email" />
+                <button type="button" class="btn btn-info" id="search-button">Search🔎</button>
+            </form>
+
+            <br />
+
+            <table class="table">
+                <caption>
+                    List of accounts that match search criteria.
+                    <form id="new-account-form" action="createaccount.html">
+                        <button id="new-account-button">+</button>
+                    </form>
+                </caption>
+                <thead>
+                    <tr>
+                        <th id="top-left" scope="col">Name</th>
+                        <th scope="col">Birthday</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Type</th>
+                        <th scope="col">Last Visit</th>
+                        <th id="top-right" scope="col">Checked Out</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td><button class="open-button">Johnathan Tryall</button></td>
+                        <td><button class="middle-button">01/07/2002</button></td>
+                        <td><button class="middle-button">j.tryall@yahoo.com</button></td>
+                        <td><button class="middle-button">Member</button></td>
+                        <td><button class="middle-button">01/23/2026</button></td>
+                        <td><button class="close-button">No</button></td>
+                    </tr>
+
+                    <tr>
+                        <td><button class="open-button">Samantha Smith</button></td>
+                        <td><button class="middle-button">07/12/2000</button></td>
+                        <td><button class="middle-button">s.smith@gmail.com</button></td>
+                        <td><button class="middle-button">Guest</button></td>
+                        <td><button class="middle-button">01/15/2026</button></td>
+                        <td><button class="close-button">Terry</button></td>
+                    </tr>
+
+                    <tr>
+                        <td><button class="open-button">Peter Quill</button></td>
+                        <td><button class="middle-button">10/30/1980</button></td>
+                        <td><button class="middle-button">starlord@hotmail.com</button></td>
+                        <td><button class="middle-button">Guest</button></td>
+                        <td><button class="middle-button">01/17/2026</button></td>
+                        <td><button class="close-button">No</button></td>
+                    </tr>
+
+                    <tr>
+                        <td><button class="open-button">Michael Jackson</button></td>
+                        <td><button class="middle-button">08/29/1958</button></td>
+                        <td><button class="middle-button">smoothcriminal@hehe.com</button></td>
+                        <td><button class="middle-button">Guest</button></td>
+                        <td><button class="middle-button">01/31/1988</button></td>
+                        <td><button class="close-button">No</button></td>
+                    </tr>
+
+                    <tr>
+                        <td id="bottom-left"><button class="open-button">End of list</button></td>
+                        <td><button class="middle-button">End of list</button></td>
+                        <td><button class="middle-button">End of list</button></td>
+                        <td><button class="middle-button">End of list</button></td>
+                        <td><button class="middle-button">End of list</button></td>
+                        <td id="bottom-right"><button class="close-button">End of list</button></td>
+                    </tr>
+                </tbody>
+            </table>
+
+            <form action="entrylookup.html">
+                <button class="btn btn-danger">Check Out Selected Account</button>
+            </form>
+          </main>
   );
 }
--- src/entrylookup/entrylookup.jsx
+++ src/entrylookup/entrylookup.jsx
@@ -1,9 +1,94 @@
 import React from 'react';
+import './entrylookup.css'
 
 export function Entrylookup() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>entrylookup displayed here</div>
-    </main>
+            <main>
+            <h1>Account Viwer</h1>
+
+            <h2>Now Viewing Peter Quill</h2>
+
+            <form id="grid-form" method="get" action="database.html">
+                <div class="row">
+                    <div class="col">
+                        <label for="inputName">First Name</label>
+                        <input type="text" class="form-control" id="inputName" value="Peter" placeholder="First Name" required />
+                    </div>
+                    <div class="col">
+                        <label for="inputMiddleName">Middle Name</label>
+                        <input type="text" class="form-control" id="inputMiddleName" value="" placeholder="Middle Name" />
+                    </div>
+                    <div class="col">
+                        <label for="inputLastName">Last Name</label>
+                        <input type="text" class="form-control" id="inputLastName" value="Quill" placeholder="Last Name" required />
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col">
+                        <label for="inputDOB">Date of Birth</label>
+                        <input type="date" class="form-control" id="inputDOB" value="1980-10-30" required />
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col">
+                        <label for="inputEmail">Email</label>
+                        <input type="email" class="form-control" id="inputEmail" value="starlord@hotmail.com" placeholder="Email" required />
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col">
+                        <label for="memberType">Type</label>
+                        <select name="type" id="memberType" class="form-control" value="Guest">
+                            <option value="Guest">Guest</option>
+                            <option value="Member">Member</option>
+                        </select>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col">
+                        <label for="lastVisit">Last Visit</label>
+                        <input class="form-control" type="date" id="lastVisit" readonly value="2026-01-17" />
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col">
+                        Notes:
+                        <div class="notes-list">
+                            <textarea class="form-control" name="note" id="note1" readonly>Likes to listen to music while climbing. -Sarah 08/24/2025</textarea>
+                            <textarea class="form-control" name="note" id="note2" readonly>Needs belay certification. -John 01/16/25</textarea>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col">
+                        <label for="new-note-form">Create New Note:</label>
+                        <textarea class="form-control" name="note" id="new-note-form" placeholder="Type new note..."></textarea>
+                    </div>
+                    <div class="col">
+                        <button type="button" id="submit-new-note" class="btn btn-warning">Save Note</button>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col">
+                        <div class="submit-buttons">
+                            <button type="submit" class="btn btn-info">Save and Check In</button>
+                        </div>
+                    </div>
+
+                    <div class="col">
+                        <div class="submit-buttons">
+                            <button type="submit" class="btn btn-danger">Exit and Check In</button>
+                        </div>
+                    </div>
+                </div>
+            </form> 
+        </main>
   );
 }

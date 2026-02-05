d5cf70083fe9208a0baa3bd0c7ea7871bba0957e
WaiveRyder <nzh.wolf@gmail.com>
2026-02-05 11:32:25 -0700

    Added simple functions to each jsx file

---
+++ src/createaccount/createaccount.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Createaccount() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>createaccount displayed here</div>
+    </main>
+  );
+}
---
+++ src/entrylookup/entrylookup.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Entrylookup() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>entrylookup displayed here</div>
+    </main>
+  );
+}

632e3f7b7a37990317049d8cd7017125439f2dbd
WaiveRyder <nzh.wolf@gmail.com>
2026-02-05 11:37:22 -0700

    Added BrowserRouter

--- src/app.jsx
+++ src/app.jsx
@@ -10,27 +10,29 @@ import { About } from './about/about';
 
 export default function App() {
   return (
-        <body>
-          <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-          <header>
-            <h1 id="page-title">BelayOn</h1>
+        <BrowserRouter>
+          <body>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+            <header>
+              <h1 id="page-title">BelayOn</h1>
 
-            <nav>
-                <menu>
-                    <li><a class="navbar-link-main" href="index.html">Login</a></li>
-                    <li><a class="navbar-link" href="database.html">Database</a></li>
-                    <li><a class="navbar-link" href="createaccount.html">Create New Account</a></li>
-                    <li><a class="navbar-link" href="entrylookup.html">View Account</a></li>
-                    <li><a class="navbar-link" href="about.html">About</a></li>
-                </menu>
-            </nav>
-          </header>
+              <nav>
+                  <menu>
+                      <li><a class="navbar-link-main" href="index.html">Login</a></li>
+                      <li><a class="navbar-link" href="database.html">Database</a></li>
+                      <li><a class="navbar-link" href="createaccount.html">Create New Account</a></li>
+                      <li><a class="navbar-link" href="entrylookup.html">View Account</a></li>
+                      <li><a class="navbar-link" href="about.html">About</a></li>
+                  </menu>
+              </nav>
+            </header>
 
-          <main>App Components go here</main>
+            <main>App Components go here</main>
 
-          <footer id="align-text">
-            <p> <span id="author">Nathan Hunt</span><a id='github-link' href="https://github.com/WaiveRyder/BelayOn" target="_blank">GitHub</a></p>
-        </footer>
-        </body>    
+            <footer id="align-text">
+              <p> <span id="author">Nathan Hunt</span><a id='github-link' href="https://github.com/WaiveRyder/BelayOn" target="_blank">GitHub</a></p>
+          </footer>
+          </body>
+        </BrowserRouter>    
   );
 }

1350dd5d881cfc58cdcf9c5f99a3c982d698b55a
WaiveRyder <nzh.wolf@gmail.com>
2026-02-05 14:38:35 -0700

    Fixed incorrect React syntax pt2

--- src/database/database.jsx
+++ src/database/database.jsx
@@ -9,13 +9,13 @@ export function Database() {
             <p>Logged in as: User. All edits will be logged under this name.</p>
 
             <form id="search" method="get">
-                <input type="search" class="form-control" id="search-box" required placeholder="Name or Email" />
-                <button type="button" class="btn btn-info" id="search-button">Search🔎</button>
+                <input type="search" className="form-control" id="search-box" required placeholder="Name or Email" />
+                <button type="button" className="btn btn-info" id="search-button">Search🔎</button>
             </form>
 
             <br />
 
-            <table class="table">
+            <table className="table">
                 <caption>
                     List of accounts that match search criteria.
                     <form id="new-account-form" action="createaccount.html">
@@ -34,54 +34,54 @@ export function Database() {
                 </thead>
                 <tbody>
                     <tr>
-                        <td><button class="open-button">Johnathan Tryall</button></td>
-                        <td><button class="middle-button">01/07/2002</button></td>
-                        <td><button class="middle-button">j.tryall@yahoo.com</button></td>
-                        <td><button class="middle-button">Member</button></td>
-                        <td><button class="middle-button">01/23/2026</button></td>
-                        <td><button class="close-button">No</button></td>
+                        <td><button className="open-button">Johnathan Tryall</button></td>
+                        <td><button className="middle-button">01/07/2002</button></td>
+                        <td><button className="middle-button">j.tryall@yahoo.com</button></td>
+                        <td><button className="middle-button">Member</button></td>
+                        <td><button className="middle-button">01/23/2026</button></td>
+                        <td><button className="close-button">No</button></td>
                     </tr>
 
                     <tr>
-                        <td><button class="open-button">Samantha Smith</button></td>
-                        <td><button class="middle-button">07/12/2000</button></td>
-                        <td><button class="middle-button">s.smith@gmail.com</button></td>
-                        <td><button class="middle-button">Guest</button></td>
-                        <td><button class="middle-button">01/15/2026</button></td>
-                        <td><button class="close-button">Terry</button></td>
+                        <td><button className="open-button">Samantha Smith</button></td>
+                        <td><button className="middle-button">07/12/2000</button></td>
+                        <td><button className="middle-button">s.smith@gmail.com</button></td>
+                        <td><button className="middle-button">Guest</button></td>
+                        <td><button className="middle-button">01/15/2026</button></td>
+                        <td><button className="close-button">Terry</button></td>
                     </tr>
 
                     <tr>
-                        <td><button class="open-button">Peter Quill</button></td>
-                        <td><button class="middle-button">10/30/1980</button></td>
-                        <td><button class="middle-button">starlord@hotmail.com</button></td>
-                        <td><button class="middle-button">Guest</button></td>
-                        <td><button class="middle-button">01/17/2026</button></td>
-                        <td><button class="close-button">No</button></td>
+                        <td><button className="open-button">Peter Quill</button></td>
+                        <td><button className="middle-button">10/30/1980</button></td>
+                        <td><button className="middle-button">starlord@hotmail.com</button></td>
+                        <td><button className="middle-button">Guest</button></td>
+                        <td><button className="middle-button">01/17/2026</button></td>
+                        <td><button className="close-button">No</button></td>
                     </tr>
 
                     <tr>
-                        <td><button class="open-button">Michael Jackson</button></td>
-                        <td><button class="middle-button">08/29/1958</button></td>
-                        <td><button class="middle-button">smoothcriminal@hehe.com</button></td>
-                        <td><button class="middle-button">Guest</button></td>
-                        <td><button class="middle-button">01/31/1988</button></td>
-                        <td><button class="close-button">No</button></td>
+                        <td><button className="open-button">Michael Jackson</button></td>
+                        <td><button className="middle-button">08/29/1958</button></td>
+                        <td><button className="middle-button">smoothcriminal@hehe.com</button></td>
+                        <td><button className="middle-button">Guest</button></td>
+                        <td><button className="middle-button">01/31/1988</button></td>
+                        <td><button className="close-button">No</button></td>
                     </tr>
 
                     <tr>
-                        <td id="bottom-left"><button class="open-button">End of list</button></td>
-                        <td><button class="middle-button">End of list</button></td>
-                        <td><button class="middle-button">End of list</button></td>
-                        <td><button class="middle-button">End of list</button></td>
-                        <td><button class="middle-button">End of list</button></td>
-                        <td id="bottom-right"><button class="close-button">End of list</button></td>
+                        <td id="bottom-left"><button className="open-button">End of list</button></td>
+                        <td><button className="middle-button">End of list</button></td>
+                        <td><button className="middle-button">End of list</button></td>
+                        <td><button className="middle-button">End of list</button></td>
+                        <td><button className="middle-button">End of list</button></td>
+                        <td id="bottom-right"><button className="close-button">End of list</button></td>
                     </tr>
                 </tbody>
             </table>
 
             <form action="entrylookup.html">
-                <button class="btn btn-danger">Check Out Selected Account</button>
+                <button className="btn btn-danger">Check Out Selected Account</button>
             </form>
           </main>
   );

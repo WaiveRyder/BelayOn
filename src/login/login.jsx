1dbc1fa4c1bd21e6638943a6c2410c67b19caff1
WaiveRyder <nzh.wolf@gmail.com>
2026-02-20 19:43:25 -0700

    Added email and password react components. Adds user to local storage

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,23 +1,34 @@
 import React from 'react';
 
 export function Login() {
+    const [email, setEmail] = React.useState("");
+    const [password, setPassword] = React.useState("");
+
+    function registerUser() {
+        localStorage.setItem("user", email)
+    }
+
+    function loginUser() {
+        localStorage.setItem("user", email)
+    }
+
   return (
     <div className="body">
         <main>
                 <h1>Welcome to BelayOn!</h1>
                 <h2>Please login or sign up below.</h2>
-                <form method="get" id='login' action="database">
+                <form method="get" id='login'>
                     <div>
                         @ Email Address
-                        <input type="email" className="form-control" required placeholder="enter email" />
+                        <input type="email" className="form-control" onChange={(e) => {setEmail(e.target.value)}} value={email} required placeholder="enter email" />
                     </div>
                     <div>
                         🔒Password
-                        <input type="enter password" className="form-control" required placeholder="password" />
+                        <input type="password" className="form-control" onChange={(e) => {setPassword(e.target.value)}} value={password} required placeholder="password" />
                     </div>
                     <div className="login-form">
-                        <button className="btn btn-outline-primary me-2">Login</button>
-                        <button className="btn btn-primary">Sign Up</button>
+                        <button className="btn btn-outline-primary me-2" onClick={loginUser} disabled={!email || !password}>Login</button>
+                        <button className="btn btn-primary" onClick={registerUser} disabled={!email || !password}>Sign Up</button>
                     </div>
                 </form>
         </main>

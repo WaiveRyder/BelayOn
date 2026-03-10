9c79cf2ce3f723b8d957c93bd057ef0cf0d47dfe
WaiveRyder <nzh.wolf@gmail.com>
2026-03-10 15:13:49 -0600

    init databases

--- service/index.js
+++ service/index.js
@@ -2,3 +2,8 @@ const express = require('express');
 const app = express();
 const uuid = require('uuid');
 const bcrypt = require('bcrypt');
+const cookieParser = require('cookie-parser');
+
+const users = [];
+
+const customers = [];

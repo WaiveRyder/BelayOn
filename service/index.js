ea9be288a8addd037b2532a8d6bedd5ce60b9ce7
WaiveRyder <nzh.wolf@gmail.com>
2026-03-10 15:38:42 -0600

    register api implemented

--- service/index.js
+++ service/index.js
@@ -14,3 +14,20 @@ app.use(express.static('public'));
 
 var apiRouter = express.Router();
 app.use('/api', apiRouter);
+
+apiRouter.post('/register', async (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    if (email === null || password === null) {
+        res.status(400).send({ msg: 'Erorr: no null fields allowed'});
+    } else if (users.find(user => user.email === email)) {
+        res.status(409).send({ msg: 'Error: email already in use'});
+    } else {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = { email: email, password: hashedPassword, authToken: uuid.v4()}
+        users.push(user);
+
+        res.cookie('authToken', user.authToken, { secure: true, httpsOnly: true, sameSite: 'strict', maxAge: 60*60*24})
+        res.status(200).send({ email: user.email });
+    }
+});

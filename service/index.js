280f12b8e34dd05468553ca5602507823eeb4310
WaiveRyder <nzh.wolf@gmail.com>
2026-03-10 17:30:01 -0600

    Implemented login endpoint

--- service/index.js
+++ service/index.js
@@ -19,16 +19,32 @@ apiRouter.post("/register", async (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
     if (email === null || password === null) {
-        res.status(400).send({ msg: "Error: no null fields allowed"});
+        res.status(400).send({msg: "Error: no null fields allowed"});
     } else if (users.find(user => user.email === email)) {
-        res.status(409).send({ msg: "Error: email already in use"});
+        res.status(409).send({msg: "Error: email already in use"});
     } else {
         const hashedPassword = await bcrypt.hash(password, 10);
         const user = { email: email, password: hashedPassword, authToken: uuid.v4()}
         users.push(user);
 
-        res.cookie('authToken', user.authToken, { secure: true, httpsOnly: true, sameSite: 'strict', maxAge: 60*60*24})
-        res.status(200).send({ email: user.email });
+        res.cookie("authToken", user.authToken, {secure: true, httpsOnly: true, sameSite: "strict", maxAge: 60*60*24});
+        res.status(200).send({email: user.email});
+    }
+});
+
+apiRouter.post("/login", async (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password
+    
+    const user = users.find(u => u.email === email);
+
+    if (user === null || password === null) {
+        res.status(400).send({msg: "Error: no null fields allowed"})
+    } else if (user.email === email && bcrypt.compare(u.password, password)) {
+        user.authToken = uuid.v4();
+        
+        res.cookie("authToken", user.authToken, {secure: true, httpsOnly: true, sameSite: "strict", maxAge: 60*60*24})
+        res.status(200).send();
     }
 });
 

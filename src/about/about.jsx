b4cb8cc9051732f6c7e858af87fd111a7f5581cc
WaiveRyder <nzh.wolf@gmail.com>
2026-03-12 09:05:55 -0600

    Fixed video cropping

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -85,15 +85,15 @@ export function About() {
                     </h2>
                     <div id="collapseFour" className="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#about-accordion">
                         <div className="accordion-body">
-                            {getQuote().quote}
-                            <p>{getQuote().author}</p>
+                            <div className="ratio ratio-1x1">
+                                <iframe width="560" height="315" src={source} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
 
             <img id="ondra" src="AdamOndraSilence.jpg" alt="Adam Ondra climbing the route Silence." />
-            <iframe width="560" height="315" src={source} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
         </main>
   );
 }

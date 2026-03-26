54195032ab3395daf5e33670a471b3e4ad79a191
WaiveRyder <nzh.wolf@gmail.com>
2026-03-26 08:46:01 -0600

    Implemented ws on message and pong in server

---
+++ service/peerProxy.js
@@ -0,0 +1,21 @@
+const {WebSocketServer, WebSocket} = require('ws');
+
+function peerProxy(server) {
+    const wss = new WebSocketServer({ server: server})
+
+    WebSocketServer.on("connection", (ws) => {
+        ws.isAlive = true;
+
+        ws.on("message", function message(data) {
+            wss.clients.forEach((client) => {
+                if (client !== ws && client.readyState === WebSocket.OPEN) {
+                    client.send(data);
+                }
+            })
+        })
+
+        ws.on("pong", () => {
+            ws.isAlive = true;
+        })
+    })
+}
